a0fc03250bb79275ef29edf43d465ae63adfb472
self_refactor <deyan8284@gmail.com>
2018-10-16 19:05:01 +0000

    feat: sort sizes

--- showFileSize.js
+++ showFileSize.js
@@ -1,7 +1,31 @@
 const filesize = require('filesize')
 const {statSync} = require('fs')
-const {remove, replace} = require('rambdax')
+const {remove, replace, sort} = require('rambdax')
 const cTable = require('console.table');
+
+const BYTES = ' B'
+const KILOBYTES = ' KB'
+
+function hasBytes(x){
+  return 
+}
+
+function sortSizes(sizes){
+  const sortFn = (aRaw,bRaw) => {
+    const a = aRaw.size
+    const b = bRaw.size
+    if(a.includes(BYTES)&&b.includes(BYTES)){
+      return Number(remove(BYTES, a)) > Number(remove(BYTES, b))
+    }
+    if(a.includes(BYTES)&&b.includes(KILOBYTES)) return -1
+    if(a.includes(KILOBYTES)&&b.includes(BYTES)) return 1
+    
+      return Number(remove(KILOBYTES, a)) > Number(remove(KILOBYTES, b))
+  }
+
+  return sort(sortFn, sizes)
+
+}
 
 const showFileSize  = async () => {
   try{
@@ -29,7 +53,7 @@ const showFileSize  = async () => {
         size: filesize(size)
       }
     })
-    console.table(sizes)
+    console.table(sortSizes(sizes))
   }catch(err){
     console.log(err)
   }
