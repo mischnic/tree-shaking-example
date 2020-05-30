be8eaf33d9538fd761bb077f73ccd5bc219925e1
Evan Wallace <evan.exe@gmail.com>
2020-05-29 21:19:19 -0700

    prevent show size from being crazy slow

--- showFileSize.js
+++ showFileSize.js
@@ -16,24 +16,6 @@ const supportedLibraries = [
 	"rambdax"
 ];
 
-COVERAGE_REGEX = /Coverage summary =+\nStatements\s*:\s*([\d.]+)%.*\nBranches\s*:\s*([\d.]+)%.*\nFunctions\s*:\s*([\d.]+)%.*\n/;
-
-function coverage(path) {
-	try {
-		const output = cp
-			.execSync(`nyc --reporter text-summary node ${path}`)
-			.toString("utf8");
-		const [, statements, branches, functions] = output.match(
-			COVERAGE_REGEX
-		);
-		return `Stmts: ${String(Math.round(statements)).padStart(
-			2
-		)}%, Functions: ${String(Math.round(functions)).padStart(2)}%`;
-	} catch {
-		return "Runtime error!";
-	}
-}
-
 try {
 	const filePaths = supportedLibraries
 		.map(xx => {
@@ -51,7 +33,6 @@ try {
 			return {
 				file: remove([`${__dirname}/`, ".js"], x),
 				size: filesize(size),
-				coverage: coverage(x)
 			};
 		})
 		.reduce((acc, v, i) => {
